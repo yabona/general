9b5cf8ec4622aa74e8a98dd1074aed5380af3828
yabona <noahbailey9214201@gmail.com>
2017-03-20 21:29:34 -0400

    Update watchlist.ps1
    Added search engine randomization

--- watchlist.ps1
+++ watchlist.ps1
@@ -1,7 +1,14 @@
 #watchlist.PS1
 [System.Collections.ArrayList]$wordlist = (Invoke-Webrequest -Uri https://raw.githubusercontent.com/yabona/general/master/__NSAKEY).Content.Split(",")
 while ($true) {
-    $search = "https://google.ca/search?q="
+    $engine = 1..5|get-random  
+    switch ($engine) {
+        1 {$search = "https://google.ca/search?q="}
+        2 {$search = "https://www.bing.com/search?q="}
+        3 {$search = "https://search.yahoo.com/search;?p="}
+        4 {$search = "https://www.ask.com/web?q="}
+        5 {$search = "https://https://baidu.com/s?ie=ie=utf-8&wd="}
+    }
     $num = 1..5|Get-random
     for ($j = 0; $j -lt $num; $j++) {
         $search += [string]($wordlist[(Get-random -Maximum $wordlist.count)]).replace(' ','%20')
