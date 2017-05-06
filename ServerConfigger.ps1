091d66982a22d5190663925ae1be21102469b1c3
yabona <noahbailey9214201@gmail.com>
2017-05-06 17:28:38 -0400

    Create ServerConfigger.ps1

---
+++ ServerConfigger.ps1
@@ -0,0 +1,54 @@
+Write-host "
+Automagic server config-er
+# Yabones 2017 
+# open sourse asf pls send this to your aunt and grandparents and stuff
+"
+
+#declare system setup workflow...
+Workflow Configure_System {
+    parm (
+        [string]$hostname,
+        [string]$domainName
+    )
+
+    Rename-Computer -NewName $hostname -force
+    
+    # restart computer, then resume on restart...
+    Restart-Computer -Wait
+
+    # domain join
+    Add-computer -DomainName $domainName -restart -Credential (Get-Credential) 
+}
+
+
+# setup hostname
+$hostName = Read-Host "Hostname"
+$domainName = Read-host "AD Domain Name"
+
+
+# Configure networking
+do {
+    write-output $(Get-netadapter)
+    $interfaceAlias = Read-Host "Network Interface"
+    $interfaceIP = Read-Host "IP Address"
+    $InterfaceSubnet = Read-Host "Bitmask"
+    $InterfaceGateway = Read-Host "Gateway"
+    $InterfaceDNS = Read-Host "Primary DNS"
+
+    # Make user check input
+    Write-Output "`nConfirm:`n`n$interfaceAlias`nIP: $InterfaceIP /$InterfaceSubnet`nGW: $InterfaceGateway`nDNS: $InterfaceDNS"
+    pause
+
+    # Apply changes
+    New-NetIPAddress -InterfaceAlias $interfaceAlias -IPAddress $interfaceIP -PrefixLength $InterfaceSubnet -DefaultGateway $InterfaceGateway
+    Set-DnsClientServerAddress -InterfaceAlias $interfaceAlias -ServerAddresses $InterfaceDNS
+
+    # check changes
+    Get-NetIPAddress | Select IPAddress,InterfaceAlias
+
+    [boolean]$continue = [boolean]$(Read-Host "Continue? [1/0]")
+
+} while ($continue) 
+
+# Proceed to call workflow
+Configure_System -hostname $hostName -domainname $domainname
